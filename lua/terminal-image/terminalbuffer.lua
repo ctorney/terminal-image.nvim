b0a20320bed58de0d538123b20152e4a67d4e44a
Colin Torney <colin.j.torney@gmail.com>
2025-03-02 15:04:02 +0000

    hello bello

--- lua/terminal-image/terminalbuffer.lua
+++ lua/terminal-image/terminalbuffer.lua
@@ -9,61 +9,82 @@ function M.new(buf)
 	local self = setmetatable({}, M)
 	self.buf = buf
 	self.cursor_row = 0
-	self.scan_line = 0
+	self.scan_line = 1
 	self.imgs = {}
 	self.idx = {}
-	local group = vim.api.nvim_create_augroup("snacks.image.inline." .. buf, { clear = true })
+	-- local group = vim.api.nvim_create_augroup("snacks.image.inline." .. buf, { clear = true })
 
-	self.events = {}
-	vim.api.nvim_buf_attach(buf, false, {
-		on_lines = function(...)
-			table.insert(self.events, { ... })
-			vim.notify("Hello from on_lines", vim.log.levels.INFO)
+	-- self.events = {}
+	-- vim.api.nvim_buf_attach(buf, false, {
+	-- 	on_lines = function(...)
+	-- 		table.insert(self.events, { ... })
+	-- 		vim.notify("Hello from on_lines", vim.log.levels.INFO)
+	-- 	end,
+	-- })
+
+	vim.api.nvim_buf_attach(buf, true, {
+		on_lines = function(bufnr, chan, changedtick, firstline, lastline, new_lastline, byte_count)
+			self:update(firstline, lastline, new_lastline, changedtick)
 		end,
 	})
+	-- local timer = assert(uv.new_timer())
 
-	local timer = assert(uv.new_timer())
+	-- local update = function()
+	-- 	return timer:start(
+	-- 		1000,
+	-- 		500,
+	-- 		vim.schedule_wrap(function()
+	-- 			self:update()
+	-- 		end)
+	-- 	)
+	-- end
 
-	local update = function()
-		return timer:start(
-			1000,
-			500,
-			vim.schedule_wrap(function()
-				self:update()
-			end)
-		)
-	end
-
-	vim.api.nvim_create_autocmd({ "BufDelete", "BufWipeout" }, {
-		group = group,
-		buffer = buf,
-		callback = function(ev)
-			vim.notify("Hello from close", vim.log.levels.INFO)
-			if ev.buf == self.buf then
-				timer:stop()
-				vim.notify("Hello from timer stop", vim.log.levels.INFO)
-			end
-		end,
-	})
+	-- vim.api.nvim_create_autocmd({ "BufDelete", "BufWipeout" }, {
+	-- 	group = group,
+	-- 	buffer = buf,
+	-- 	callback = function(ev)
+	-- 		vim.notify("Hello from close", vim.log.levels.INFO)
+	-- 		if ev.buf == self.buf then
+	-- 			timer:stop()
+	-- 			vim.notify("Hello from timer stop", vim.log.levels.INFO)
+	-- 		end
+	-- 	end,
+	-- })
 	-- vim.schedule(update)
 	return self
 end
 
-function M:update()
-	vim.notify("Hello from image update", vim.log.levels.INFO)
-	vim.notify(self.events, vim.log.levels.INFO)
+print(" hello from terminalbuffer")
+function M:update(firstline, lastline, new_lastline, changedtick)
+	-- vim.notify(
+	-- 	"Hello " .. firstline .. " " .. lastline .. " " .. new_lastline .. " " .. changedtick,
+	-- 	vim.log.levels.INFO
+	-- )
+	-- vim.notify("Hello from image update", vim.log.levels.INFO)
+	-- vim.notify(self.events, vim.log.levels.INFO)
 	-- return
 	-- get number of lines in the buffer
-	local lines = vim.api.nvim_buf_line_count(self.buf)
+	-- local lines = vim.api.nvim_buf_line_count(self.buf)
 
-	if self.scan_line < lines then
-		-- loop through the lines
-		for i = self.scan_line, lines do
-			-- get the line
-			local line = vim.api.nvim_buf_get_lines(self.buf, i, i + 1, false)[1]
-			-- check if the line contains an image
-			-- local img = line:match("![%[]")
-			-- if img then
+	-- loop through the lines
+	for i = firstline + 1, lastline do
+		-- get the line
+		local line = vim.api.nvim_buf_get_lines(self.buf, i, i + 1, false)[1]
+		if not line then
+			break
+		end
+		-- check if the line contains an image
+		local img = line:match("^hello")
+		if img then
+			-- line = line:gsub("hello", "world")
+			local ns = vim.api.nvim_create_namespace("my_terminal_marks")
+			vim.api.nvim_buf_set_extmark(self.buf, ns, i, 0, {
+				virt_text = { { "bello", "Comment" } },
+				virt_text_pos = "overlay",
+				hl_mode = "combine",
+				priority = 10,
+			})
+			-- vim.api.nvim_buf_set_lines(self.buf, i, i + 1, false, { line })
 			--   -- get the image path
 			--   local path = line:match("%((.+)%)")
 			--   if path then
@@ -74,11 +95,11 @@ function M:update()
 			--     -- add the index to the list
 			--     self.idx[i] = #self.imgs
 			--   end
-			-- end
-			-- vim.notify("scanning line " .. i, vim.log.levels.INFO)
+			-- vim.notify("found on  " .. i, vim.log.levels.INFO)
 		end
-		self.scan_line = lines
+		-- vim.notify("scanning line " .. i, vim.log.levels.INFO)
 	end
+	self.scan_line = firstline
 	-- local win_id = vim.fn.bufwinid(self.buf)
 	-- if win_id ~= -1 then
 	-- get the current cursor position
